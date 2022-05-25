e87bf3bcbe4285bd2766b932652b704e04c4041f
xande <xanderjennie21@gmail.com>
2022-05-25 21:46:51 +0200

    CreateAccount & Login method

--- main.java
+++ main.java
@@ -1,5 +1,33 @@
+import java.util.*;
 class Program {
+    static int cardno;
+    static int password;
+
     public static void main(String[] args) {
-        System.out.println("test");
+        CreateAccount();
+        Login(cardno,password);
+    }
+    public static void CreateAccount(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Account setup");
+        System.out.println("Card Number:");
+        int cardno = sc.nextInt();
+        System.out.println("Password:");
+        int password = sc.nextInt();
+    }
+    public static void Login(int cardno, int password){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Log in:");
+        System.out.println("Card Number:");
+        int fcardno = sc.nextInt();
+        System.out.println("Password:");
+        int fpassword = sc.nextInt();
+
+        if (fcardno == cardno && fpassword == password){
+            System.out.println("logged in");
+        }
+        else {
+            System.out.println("Incorrect credentials");
+        }
     }
 }
